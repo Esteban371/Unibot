66ef904b141fbf8f5da76d5a8ca5cb97fe787e75
Esteban371 <65819788+Esteban371@users.noreply.github.com>
2026-04-07 16:20:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const SYSTEM_PROMPT = 'Eres UniBot, asistente de Unicomfacauca. Responde claro, profesional y en español.';
 
